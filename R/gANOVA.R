f5f19ae8856e670e5aec6e5781a734677e8aa8f2
jaromilfrossard <jaromil.frossard@hotmail.com>
2018-05-15 13:29:03 +0200

    class

--- R/gANOVA.R
+++ R/gANOVA.R
@@ -73,6 +73,7 @@ gANOVA <- function (formula, data = NULL, REML = TRUE, control = lmerControl(),
   res <- lmerTest:::as_lmerModLT(model, devfun)
 
   res@call <- mc
+  res <- as(res, c("lmerModgANOVA"))
   res
 
   # model <<- model
